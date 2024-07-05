6aa61624e2939721145765c27a97833fa3647a9d
pranav kumar <prans7590@gmail.com>
2024-07-05 17:30:51 +0530

    o-Auth

---
+++ src/test/java/com/o/auth/OAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.o.auth;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
